a2ce3e2b484f23230273fa5fb4237d958be47275
zhaighin <zhaighin@hotmail.com>
2021-04-19 08:09:47 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import Login from './login/login';
+import './App.css'
+import { Layout } from 'antd';
 
-function App() {
+const { Header, Footer, Sider, Content } = Layout;
+
+const App: FC = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="app-wrapper">
+      <Header>
+        Header
+      </Header>
+      <Content className="content-wrapper">
+        <Login/>
+      </Content>
+      <Footer>
+        Footer
+      </Footer>
+    </Layout>
   );
+
 }
 
 export default App;
---
+++ src/login/login.tsx
@@ -0,0 +1,57 @@
+import { Button, Checkbox, Form, Input } from 'antd';
+import React, { FC, useState } from 'react';
+import './login.css';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+const Login: FC = () => {
+  return (
+    <div className="login-container">
+      <h2>Log In</h2>
+      <Form
+        name="normal_login"
+        initialValues={{ remember: true }}
+      >
+        <Form.Item
+          className="login-form"
+          name="username"
+          rules={[{ required: true, message: 'Enter your username' }]}
+        >
+          <Input prefix={<UserOutlined/>} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          className="login-form"
+          name="password"
+          rules={[{ required: true, message: 'Enter your password' }]}
+        >
+          <Input
+            prefix={<LockOutlined/>}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <div className="login-form-password">
+          <a href="" >
+            Forgot password?
+          </a>
+        </div>
+        <Form.Item >
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+        </Button>
+        </Form.Item>
+
+        <Form.Item>
+          Don't have an account?&nbsp;
+          <a className="login-form-signup" href="">
+            Sign up
+          </a>
+        </Form.Item>
+      </Form>
+
+    </div>
+
+
+  );
+}
+
+export default Login;
